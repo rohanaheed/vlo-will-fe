ce15e1f85a5f3a57d5c1fa98e4df734d50c398f1
Ali <aliaswadmaan34@gmail.com>
2026-02-04 12:23:56 +0500

    Login api integrated

---
+++ components/common/Loader.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Loader() {
+    return (
+        <div className="flex items-center justify-center gap-2">
+            <svg
+                className="animate-spin h-5 w-5 text-white"
+                viewBox="0 0 24 24"
+            >
+                <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                    fill="none"
+                />
+                <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                />
+            </svg>
+        </div>
+    )
+}
